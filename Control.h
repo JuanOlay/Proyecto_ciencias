be6b2d0d9215052727a9f698261570f8a8788a42
jpmc200666 <153383651+jpmc200666@users.noreply.github.com>
2025-06-08 21:17:41 -0500

    Add files via upload

--- Control.h
+++ Control.h
@@ -2,16 +2,11 @@
 #define CONTROL_H
 #include <iostream>
 #include <string>
+#include "Lista.h"
 using namespace std;
 
-struct Libro {
-    string nombre;
-    string tipoPoesia;
-    int idAutor;
-};
-
 struct Edicion {
-    int numeroEdicion;
+    string numeroEdicion;
     int fechaPublicacion;
     int idEditorial;
     string ciudadPublicacion;
@@ -38,15 +33,31 @@ struct Editorial {
     string paisOficinaP;
 };
 
+struct Libro {
+    string nombre;
+    string tipoPoesia;
+    int idAutor;
+    Lista<Edicion> ediciones;
+};
+
 class Control {
 private:
     int rtaUsuario;
+    string rtaUsuarioStr;
+    Libro libro;
+    Edicion edicion;
+    Autor autor;
+    Editorial editorial;
+    Lista<Libro> libros; 
+    Lista<Autor> autores;
+    Lista<Editorial> editoriales;
 public:
     Control(); 
     void menuPrincipal();
     void menuAgregarLibro();
     void menuEliminarLibro();
     void menuModificarLibro();
+    string menuTipoPoesia();
     void menuAgregarAutor();
     void menuEliminarAutor();
     void menuModificarAutor();
@@ -56,6 +67,22 @@ public:
     void menuAgregarEdicion();
     void menuEliminarEdicion();
     void menuModificarEdicion();
+    void menuEscogerLibro();
+    int menuEscogerAutorInsercion();
+    int menuEscogerAutorModificacion();
+    void menuEscogerEditorialInsercion();
+    void menuEscogerEditorialModificacion();
+    void menuModificacionListadoEdiciones();
+    void menuEscogerEdicionModificacion();
+    void menuEscogerEdicionEliminacion();
+    string menuSeleccionarSexoAutorInsercion();
+    void menuSeleccionarSexoAutorModificacion();
+    string menuSeleccionarFormacionAutorInsercion();
+    void menuSeleccionarFormacionAutorModificacion();
+    void imprimirListaLibros(Lista<Libro>& lista);
+    void imprimirListaAutores(Lista<Autor>& lista);
+    void imprimirListaEditoriales(Lista<Editorial>& lista);
+    //void imprimirListaEdiciones(Lista<Edicion>& Lista);
 };
 
 #endif
